84e985114354b899f8a4fbe51e078ce7433fba73
JavaHunk2020 <javahunk2020@gmail.com>
2023-06-22 06:06:07 +0530

    rest api for signup

--- src/main/java/com/kuebiko/rest/controller/SignupRestController.java
+++ src/main/java/com/kuebiko/rest/controller/SignupRestController.java
@@ -3,10 +3,12 @@ package com.kuebiko.rest.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kuebiko.dto.SignupDTO;
@@ -19,6 +21,19 @@ public class SignupRestController {
 	
 	@Autowired
 	private SignupService signupService;
+	
+	
+	@PostMapping("/csignup")
+	public AppResponse createSignup(@RequestParam String username, 
+			@RequestParam String gender,@RequestParam String email) {
+		    //below method will save data inside database
+		   signupService.persist(username, email, gender);
+			AppResponse appResponse=new AppResponse();
+			appResponse.setCode("success");
+			appResponse.setMessage("Signup is done successfully.");
+			return appResponse;
+	}
+	
 	
 	@PostMapping("/cauth")
 	public AppResponse  postLogin(@RequestBody SignupRequest signupRequest) {
