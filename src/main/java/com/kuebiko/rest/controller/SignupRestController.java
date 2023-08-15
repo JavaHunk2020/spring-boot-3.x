c61d4d9fae1c7654173a88aff82a108aae5a092f
JavaHunk2020 <javahunk2020@gmail.com>
2023-08-16 02:46:45 +0530

    email integration with spring boot

--- src/main/java/com/kuebiko/rest/controller/SignupRestController.java
+++ src/main/java/com/kuebiko/rest/controller/SignupRestController.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
